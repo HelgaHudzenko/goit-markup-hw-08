1ef5dd35bcdebc5bcc74b0ae8174bf88776eb7c9
HelgaHudzenko <olichka.og85@gmail.com>
2023-01-22 18:52:48 +0200

    edit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,16 +6,15 @@
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('backdrop--hidden');
+    mobileMenu.classList.toggle('mob-backdrop--hidden');
 
     const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.add('backdrop--hidden');
